b0f9278434d22489d371f18f20f8af6b95c326dd
GLEIPNIR\Rev. Jake <jake.blackwell.dev@gmail.com>
2026-04-05 17:55:08 -0500

    rebuilt ItemQuantity.Vue to use NumberFormGroup.vue;

--- src/entities/items/components/Physical/sheet/components/ItemQuantity.vue
+++ src/entities/items/components/Physical/sheet/components/ItemQuantity.vue
@@ -1,80 +1,50 @@
 <template>
-  <FormGroup
+  <NumberFormGroup
     label="Quantity"
     field-path="system.quantity"
+    :value="quantity"
     :default-visibility="everyoneVisibility"
     :default-editability="normalEditability"
+    :disabled="isInfinite"
+    direct-update
   >
     <template #controls="{ editable }">
       <button
         v-if="editable"
-        class="field-control-btn infinite-toggle"
+        class="field-control-btn"
         type="button"
-        @click="toggleInfinite"
         :class="{ 'is-active': isInfinite }"
         :title="'Is Infinite'"
+        @click="toggleInfinite"
       >
         <i class="fas fa-infinity" />
       </button>
     </template>
-    
-    <span v-if="isInfinite">
-      <i class="fas fa-infinity" />
-    </span>
-
-    <input
-      v-else
-      type="number"
-      :value="quantity"
-      :disabled="!isEditViewMode"
-      @change="onQuantityChange(($event.target as HTMLInputElement).value)"
-      min=0
-    />
 
     <template #readonly>
-      <span v-if="isInfinite">
-        <i class="fas fa-infinity" />
-      </span>
+      <span v-if="isInfinite"><i class="fas fa-infinity" /></span>
       <span v-else>{{ quantity }}</span>
     </template>
-  </FormGroup>
+  </NumberFormGroup>
 </template>
 
 <script setup lang="ts">
-  import { DocumentSheetStoreSymbol, RenderModeStore, RenderModeStoreSymbol } from '@ec/CoreMixin/index.mjs';
+  import { DocumentSheetStoreSymbol } from '@ec/CoreMixin/index.mjs';
   import type { PhysicalDocumentStore } from '@items/components/Physical/index.mjs';
   import {
     everyoneVisibility,
-    FormGroup,
     normalEditability,
+    NumberFormGroup,
   } from '@vc/Fields/index.mjs';
   import { computed, inject } from 'vue';
 
-  const { isEditViewMode } = inject(RenderModeStoreSymbol) as RenderModeStore;
   const {
-    documentGetters: {
-      quantity,
-    },
-    documentActions: {
-      getDirectFieldUpdater,
-    },
+    documentGetters: { quantity },
+    documentActions: { getDirectFieldUpdater },
   } = inject(DocumentSheetStoreSymbol) as PhysicalDocumentStore;
+
   const updater = getDirectFieldUpdater('system.quantity');
-
-  const toggleInfinite = () => {
-    const newValue = isInfinite.value
-      ? 0
-      : -1;
-    updater(newValue);
-  };
-
   const isInfinite = computed(() => quantity.value === -1);
 
-  const onQuantityChange = (input: string) => {
-    let value = input === '' ? 0 : Number(input);
-    if (isNaN(value)) {
-      value = 0;
-    }
-    updater(value);
-  };
+  const toggleInfinite = () => updater(isInfinite.value ? 0 : -1);
 </script>
